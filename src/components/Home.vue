0130c9333c7ebd59de3a75e1ab3ed9f5cadb75b3
Kevin <pidersk@gmail.com>
2023-02-14 21:48:39 -0500

    <enhance> finished frame of nav bar and main page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,78 @@
 <template>
-  <div class="container">你好</div>
+  <navi>
+  </navi>
+  <div class="home_container">
+    <div class="inner_container">
+      <div class="rotate_pic">
+        <img :src=pic alt="cannot find the picture!">
+      </div>
+      <div class="addition_text">
+        <p>{{addition_info}}</p>
+      </div>
+      <div class="info_section">
+        <div class="info" v-for="data in sections" :key="data.name">
+          {{data.name}}
+        </div>
+      </div>
+    </div>
+
+
+  </div>
 </template>
 
 <script>
+import picture from "../assets/main_rotate.png"
+import Navigation from "@/components/Navigation";
 export default {
   name: "Home",
   data(){
     return{
-
+      pic:picture,
+      addition_info:"Additional Information about the website and what we do",
+      sections:[
+        {name:"General", pic:""},
+        {name:"Academics", pic:""},
+        {name:"ECS", pic:""},
+      ]
     }
+  },
+  components: {
+    "navi": Navigation,
   }
 }
 </script>
 
 <style scoped>
-.container{
+.inner_container{
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+}
+.home_container{
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  align-items: center;
+}
+.rotate_pic{
 
+}
+.addition_text{
+  height: 50px;
+}
+.info_section{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  height: 500px;
+}
+.info{
+  width: 20%;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: darkgray;
 }
 </style>
---
+++ src/components/util/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="search_container">Search</div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar"
+}
+</script>
+
+<style scoped>
+.search_container{
+  width:100px;
+  border-bottom-style: solid;
+  border-width: 1px;
+  font-size: 12px;
+  text-align: left;
+  margin:0;
+}
+</style>
